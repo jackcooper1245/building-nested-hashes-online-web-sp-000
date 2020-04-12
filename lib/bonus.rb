e2f22942d6ba05ba0240830f41ec8b8fce8b3e82
Jack Cooper <jackcooper1245@gmail.com>
2020-04-12 10:28:15 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -20,7 +20,8 @@ def bonus
    }
   }
 
-epic_tragedy.has_key?()
+epic_tragedy[:montague][:hero][status:] = "dead" 
+
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
